b0196e9cdff3ea0e0b2b443d1094529777a20e7a
Caleb Johnson <cjo3484@wgu.edu>
2025-11-05 15:45:51 -0800

    Added a volume bar and volume icon with complete functionality. Included some functions 'handleVolumeSlider(), muteAudio(), and audioSeeker() to includes functionality within the audio bar and icon.

--- musicPlayer.js
+++ musicPlayer.js
@@ -1,5 +1,12 @@
 let isPlaying = false;
 
+const playIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-play-icon lucide-play"><path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z"/></svg>';
+const pauseIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pause-icon lucide-pause"><rect x="14" y="3" width="5" height="18" rx="1"/><rect x="5" y="3" width="5" height="18" rx="1"/></svg>';
+
+const volumeLowIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-volume1-icon lucide-volume-1"><path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z"/><path d="M16 9a5 5 0 0 1 0 6"/></svg>';
+const volumeHighIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-volume2-icon lucide-volume-2"><path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z"/><path d="M16 9a5 5 0 0 1 0 6"/><path d="M19.364 18.364a9 9 0 0 0 0-12.728"/></svg>';
+const mutedIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-volume-x-icon lucide-volume-x"><path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z"/><line x1="22" x2="16" y1="9" y2="15"/><line x1="16" x2="22" y1="9" y2="15"/></svg>';
+
 function getTitle() {
     var audio = document.getElementById("player");
     var src = audio.getElementsByTagName("source")[0].getAttribute("src");
@@ -11,12 +18,21 @@ function playMusic() {
     var audio = document.getElementById("player");
     audio.play();
     isPlaying = true;
+    var playPauseBtn = document.getElementById("playPauseButton");
+    playPauseBtn.innerHTML = pauseIcon;
 }
 
 function pauseMusic() {
     var audio = document.getElementById("player");
     audio.pause();
     isPlaying = false;
+    var playPauseBtn = document.getElementById("playPauseButton");
+    playPauseBtn.innerHTML = playIcon;
+}
+
+function previousTrack() {
+    var audio = document.getElementById("player");
+    audio.currentTime = 0;
 }
 
 function playPauseMusic() {
@@ -43,11 +59,15 @@ function fileSelect() {
         var audio = document.getElementById('player');
         audio.src = URL.createObjectURL(file);
         document.getElementById("song-title").textContent = file.name.replace(/\.[^/.]+$/, '');
+        
         audio.addEventListener('loadedmetadata', function() {
             getAudioDuration();
+
             var progressBar = document.getElementById("progress-bar");
             progressBar.max = audio.duration;
-            progressBar.hidden = false;
+            progressBar.removeAttribute('hidden');
+
+            document.getElementById('time').textContent = "0:00";
         });
     });
 }
@@ -67,6 +87,60 @@ function updateProgress() {
     }
 }
 
+function handleVolumeSlider() {
+    var audio = document.getElementById("player");
+    var volumeSlider = document.getElementById("volume-slider");
+    var volumeIcon = document.getElementById("volume-icon");
+
+    volumeSlider.addEventListener('input', function() {
+        audio.volume = volumeSlider.value / 100;
+        if ((volumeSlider.value == 0)) {
+            volumeIcon.innerHTML = mutedIcon;
+        }
+        else if ((volumeSlider.value) < 50) {
+            volumeIcon.innerHTML = volumeLowIcon;
+        } else {
+            volumeIcon.innerHTML = volumeHighIcon;
+        }
+    });
+}
+
+function muteAudio() {
+    var audio = document.getElementById("player");
+    var volumeSlider = document.getElementById("volume-slider");
+    var volumeIcon = document.getElementById("volume-icon");
+
+    if (audio.volume > 0) {
+        previousVolume = audio.volume;
+        audio.volume = 0
+        volumeSlider.value = 0;
+        volumeIcon.innerHTML = mutedIcon;
+    } else {
+        audio.volume = previousVolume;
+        volumeSlider.value = previousVolume * 100;
+        if (previousVolume < 0.5) {
+            volumeIcon.innerHTML = volumeLowIcon;
+        } else {
+            volumeIcon.innerHTML = volumeHighIcon;
+        }
+    }
+}
+
+function audioSeeker() {
+    var audio = document.getElementById("player");
+    var progressBar = document.getElementById("progress-bar");
+
+    progressBar.addEventListener('input', function() {
+        audio.currentTime = progressBar.value;
+    });
+}
+
 window.onload = function() {
     fileSelect();
+    audioSeeker();
+    handleVolumeSlider();
+
+    var audio = document.getElementById("player");
+    var volumeSlider = document.getElementById("volume-slider")
+    audio.volume = volumeSlider.value / 100;
 }
