6dcad4226347be1f3d7801f29c94c1364cf4dc7b
Caleb Johnson <cjo3484@wgu.edu>
2025-12-10 21:42:31 -0800

    Added a visible layout for music tracks on the left side without having any music tracks present. If you do add music tracks, you can click on the 'Music Tracks' text on top to hide all the tracks. Updated code for music tracks to make it more legible

--- src/musicPlayer.js
+++ src/musicPlayer.js
@@ -1,3 +1,5 @@
+////@ts-check
+
 let isPlaying = false;
 
 const playIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z"/></svg>';
@@ -158,8 +160,7 @@ function handleVolumeSlider() {
         audio.volume = volumeSlider.value / 100;
         volumeSlider.value == 0 ? volumeIcon.innerHTML = mutedIcon :
             volumeSlider.value < 50 ? volumeIcon.innerHTML = volumeLowIcon : volumeIcon.innerHTML = volumeHighIcon;
-    }
-    );
+    });
 }
 
 function muteAudio() {
@@ -187,6 +188,11 @@ function audioSeeker() {
     progressBar.addEventListener('input', function() {
         audio.currentTime = progressBar.value;
     });
+}
+
+function hideTracks() {
+    tracks = document.getElementById("music-tracks");
+    !tracks.hidden ? tracks.hidden = true : tracks.hidden = false;
 }
 
 function showSettingsList() {
